379177a217342a5154c4d380a313be50975eff19
bushidoagenciainfo-lab <bushido.agencia.info@gmail.com>
2026-07-31 15:03:38 -0500

    Disable letterbox transition

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,32 +1,7 @@
-"use client";
-
-import { useEffect, useState } from "react";
-
 export default function Template({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const [showLetterbox, setShowLetterbox] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowLetterbox(false);
-    }, 800); // mismo tiempo que la animación CSS
-
-    return () => clearTimeout(timer);
-  }, []);
-
-  return (
-    <>
-      {children}
-
-      {showLetterbox && (
-        <>
-          <div className="lb lb-top" aria-hidden="true" />
-          <div className="lb lb-bot" aria-hidden="true" />
-        </>
-      )}
-    </>
-  );
+  return <>{children}</>;
 }
